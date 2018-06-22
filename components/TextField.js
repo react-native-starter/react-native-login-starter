6b78cbb830225d1587b349e354b86fe8231cd685
Girish Rawat <Emailgirishrawat@gmail.com>
2018-06-22 00:12:59 -0700

    ⌨️  added password type to TextField

--- components/TextField.js
+++ components/TextField.js
@@ -2,17 +2,6 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput, ViewPropTypes } from 'react-native';
 import PropTypes from 'prop-types';
 
-
-// const propTypes = {
-//   text: PropTypes.string,
-//   type: PropTypes.string,
-//   shape: PropTypes.string,
-//   textStyle: Text.propTypes.style,
-//   buttonStyle: PropTypes.any,
-//   disabled: PropTypes.bool,
-//   onPress: PropTypes.func,
-// };
-
 const propTypes = {
     placeholder: PropTypes.string,
     shape: PropTypes.string,
@@ -20,16 +9,8 @@ const propTypes = {
 
 };
 
-// const defaultProps = {
-//   type: 'primary',
-//   shape: 'round',
-//   text: 'Get Started',
-//   disabled: false,
-// };
-
 export default class TextField extends React.Component {
     static propTypes = propTypes;
-    //   static defaultProps = defaultProps;
 
     constructor(props) {
         super(props);
@@ -44,76 +25,18 @@ export default class TextField extends React.Component {
         const { placeholder, shape, type } = this.props;
         let secureEntry = false;
         let shapeStyle;
-        
+
         if (type === 'password') {
             secureEntry = true;
         }
 
-        // let shapeStyle;
-        // if (['round', 'circle'].includes(shape)) {
-        //   shapeStyle = {
-        //     borderRadius: 5,
-        //   };
-        // }
-        // let content;
-        // if (type === 'primary') {
-        //   content = (
-        //     <View
-        //       style={[
-        //         styles.defaultButton,
-        //         buttonStyle,
-        //         shapeStyle
-        //       ]}>
-        //       <Text style={[styles.defaultText, textStyle]}>{text}</Text>
-        //     </View>
-        //   );
-        // }
-        // else if (type === 'outline') {
-        //   content = (
-        //     <View
-        //       style={[
-        //         buttonStyle,
-        //         shapeStyle, 
-        //         styles.defaultButton,
-        //         styles.outline
-        //       ]}>
-        //       <Text style={[styles.defaultText, textStyle]}>{text}</Text>
-        //     </View>
-        //   );
-        // }
-        // else if (type === 'twitter') {
-        //   content = (
-        //     <View
-        //       style={[
-        //         buttonStyle,
-        //         shapeStyle,
-        //         styles.defaultButton,
-        //         styles.twitter
-        //       ]}>
-        //       <Text style={[styles.defaultText, textStyle]}>{text}</Text>
-        //     </View>
-        //   );
-        // }
-        // else if (type === 'gmail') {
-        //   content = (
-        //     <View
-        //       style={[
-        //         buttonStyle,
-        //         shapeStyle,
-        //         styles.defaultButton,
-        //         styles.gmail
-        //       ]}>
-        //       <Text style={[styles.defaultText, textStyle]}>{text}</Text>
-        //     </View>
-        //   );
-        // }
         return (
 
             <TextInput
                 style={styles.defaultTextField}
                 onChangeText={(text) => this.setState({ text })}
                 placeholder={placeholder}
-                secureTextEntry= {secureEntry}
+                secureTextEntry={secureEntry}
             />
 
         );
